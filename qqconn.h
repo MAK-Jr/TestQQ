c2d9cf6dbb20cc8c904d1f86ea6f723379f5fd25
David MAK <DavidMak@MAK-Davidteki-MacBook-Pro.local>
2011-01-26 18:01:13 +0800

    First Commit.

---
+++ qqconn.h
@@ -0,0 +1,10 @@
+#ifndef QQ_CONN_H_
+#define QQ_CONN_H_
+
+
+int qqconn_connect( qqclient* qq );
+void qqconn_load_serverlist();
+void qqcofile://localhost/Users/DavidMak/Projects/myqq/myqq3/src/qqcrypt.hnn_get_server(qqclient* qq);
+int qqconn_establish( qqclient* qq );
+
+#endif //QQ_CONN_H_
